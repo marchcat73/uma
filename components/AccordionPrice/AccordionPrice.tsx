db17a0a3d33c7f7e84d4cc3a4dd995024f8d3c53
Vitaliy Vanyushin <wm@codehtml.ru>
2025-01-23 15:17:07 +0400

    form

--- components/AccordionPrice/AccordionPrice.tsx
+++ components/AccordionPrice/AccordionPrice.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Typography, TextField, MenuItem } from '@mui/material';
+'use client';
+import React, { useState } from 'react';
+import { Typography, TextField, MenuItem, Button } from '@mui/material';
 import styles from './AccordionPrice.module.css';
 
 const AccordionPrice = ({ priceData }: any) => {
+  const [material, setMaterial] = useState('');
+  const [articul, setArticul] = useState('');
   const { text, price, materials, articuls } = priceData;
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(material, articul);
+  };
+
+  const handleClear = (e: any) => {
+    e.preventDefault();
+    setMaterial('');
+    setArticul('');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Typography variant="subtitle1">
         {text}: {price}
       </Typography>
@@ -14,7 +29,9 @@ const AccordionPrice = ({ priceData }: any) => {
         select
         label="Материал"
         defaultValue="EvoGloss"
+        value={material}
         className={styles.textField}
+        onChange={(option) => setMaterial(option.target.value)}
       >
         {materials.map((option: any) => (
           <MenuItem key={option.value} value={option.value}>
@@ -27,7 +44,9 @@ const AccordionPrice = ({ priceData }: any) => {
         select
         label="Артикул"
         defaultValue="P001"
+        value={articul}
         className={styles.textField}
+        onChange={(option) => setArticul(option.target.value)}
       >
         {articuls.map((option: any) => (
           <MenuItem key={option.value} value={option.value}>
@@ -35,6 +54,24 @@ const AccordionPrice = ({ priceData }: any) => {
           </MenuItem>
         ))}
       </TextField>
+      <div className={styles.buttonsBox}>
+        <Button
+          variant="outlined"
+          size="large"
+          className={styles.button}
+          onClick={handleClear}
+        >
+          УДАЛИТЬ
+        </Button>
+        <Button
+          variant="contained"
+          size="large"
+          className={styles.button}
+          type="submit"
+        >
+          СОХРАНИТЬ
+        </Button>
+      </div>
     </form>
   );
 };
