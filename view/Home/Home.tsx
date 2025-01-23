1ad619c3f9177033e64b570bde2fdb327a0401e1
Vitaliy Vanyushin <wm@codehtml.ru>
2025-01-23 19:48:15 +0400

    group panels

--- view/Home/Home.tsx
+++ view/Home/Home.tsx
@@ -6,21 +6,51 @@ import {
 } from '@/components';
 
 const Home = ({ priceData, panels }: any) => {
+  function groupPanels(panels: any) {
+    const result: any = {};
+    panels.forEach((panel: any) => {
+      const key = `${panel.width}_${panel.height}`;
+      if (!result[key]) {
+        result[key] = [];
+      }
+      result[key].push(panel);
+    });
+    return Object.values(result);
+  }
+
+  const resGroupPanels = groupPanels(panels);
+
   return (
     <div>
       <AccordionTransition
         text="Итого: 12 560р."
         body={<AccordionPrice priceData={priceData} />}
       />
-      {panels &&
-        panels.map((p: any) => {
-          if (p.children) {
-            const text = `${p.width}x${p.height} - ${p.amount} шт.`;
+      {resGroupPanels &&
+        resGroupPanels.map((item: any, index: number) => {
+          // if (p.children) {
+          //   const text = `${p.width}x${p.height} - ${p.amount} шт.`;
+          //   return (
+          //     <AccordionTransition
+          //       key={`${index}`}
+          //       text={text}
+          //       body={<AccordionPanels />}
+          //     />
+          //   );
+          // } else {
+          //   return <div key={`${index}`}>panel</div>;
+          // }
+          const text = `${item[0].width}x${item[0].height} - ${item.length} шт.`;
+          if (item.length > 1) {
             return (
-              <AccordionTransition text={text} body={<AccordionPanels />} />
+              <AccordionTransition
+                key={`${index}`}
+                text={text}
+                body={<AccordionPanels />}
+              />
             );
           } else {
-            return <div>panel</div>;
+            return <div key={`${index}`}>{text}</div>;
           }
         })}
     </div>
