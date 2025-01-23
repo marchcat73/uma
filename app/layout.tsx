e112d3ae82a7c0f1874efbcdb5458c1b201a8cc5
Vitaliy Vanyushin <wm@codehtml.ru>
2025-01-23 09:23:35 +0400

    test linux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
+import '@/assets/css/styles.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './globals.css';
 import { ApolloClientProvider } from '@/components/ApolloClientProvider';
 
 export const metadata: Metadata = {
