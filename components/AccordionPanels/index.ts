fd1f716f6ab8856c6dcc6a76d3dca03a62ad95e8
Vitaliy Vanyushin <wm@codehtml.ru>
2025-01-23 16:04:04 +0400

    AccordionPanels

---
+++ components/AccordionPanels/index.ts
@@ -0,0 +1,1 @@
+export { default as AccordionPanels } from './AccordionPanels';
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
+export * from './AccordionPanels';
 export * from './AccordionPrice';
 export * from './AccordionTransition';
 export * from './ApolloClientProvider';
